b841882c9458eff532975d5536985d01507e3068
auhnij <110024481+jinhuaDu@users.noreply.github.com>
2024-12-17 15:33:58 +0800

    add /gpts

--- backend/open_webui/main.py
+++ backend/open_webui/main.py
@@ -1154,30 +1154,77 @@ async def get_models(user=Depends(get_verified_user)):
     ]
 
     # Filter out models that the user does not have access to
-    if user.role == "user":
-        filtered_models = []
-        for model in models:
-            if model.get("arena"):
-                if has_access(
-                    user.id,
-                    type="read",
-                    access_control=model.get("info", {})
-                    .get("meta", {})
-                    .get("access_control", {}),
-                ):
-                    filtered_models.append(model)
-                continue
+    # if user.role == "user":
+    #     filtered_models = []
+    #     for model in models:
+    #         if model.get("arena"):
+    #             if has_access(
+    #                 user.id,
+    #                 type="read",
+    #                 access_control=model.get("info", {})
+    #                 .get("meta", {})
+    #                 .get("access_control", {}),
+    #             ):
+    #                 filtered_models.append(model)
+    #             continue
 
-            model_info = Models.get_model_by_id(model["id"])
-            if model_info:
-                if user.id == model_info.user_id or has_access(
-                    user.id, type="read", access_control=model_info.access_control
-                ):
-                    filtered_models.append(model)
-        models = filtered_models
+    #         model_info = Models.get_model_by_id(model["id"])
+    #         if model_info:
+    #             if user.id == model_info.user_id or has_access(
+    #                 user.id, type="read", access_control=model_info.access_control
+    #             ):
+    #                 filtered_models.append(model)
+    #     models = filtered_models
 
     return {"data": models}
 
+@app.get("/api/gpts")
+async def get_gpts(user=Depends(get_verified_user)):
+    models = await get_all_models()
+
+    # Filter out filter pipelines
+    models = [
+        model
+        for model in models
+        if "pipeline" not in model or model["pipeline"].get("type", None) != "filter"
+    ]
+
+    model_order_list = webui_app.state.config.MODEL_ORDER_LIST
+    if model_order_list:
+        model_order_dict = {model_id: i for i, model_id in enumerate(model_order_list)}
+        # Sort models by order list priority, with fallback for those not in the list
+        models.sort(
+            key=lambda x: (model_order_dict.get(x["id"], float("inf")), x["name"])
+        )
+
+    # # Filter out models that the user does not have access to
+    # if user.role == "user" and not BYPASS_MODEL_ACCESS_CONTROL:
+    #     filtered_models = []
+    #     for model in models:
+    #         if model.get("arena"):
+    #             if has_access(
+    #                 user.id,
+    #                 type="read",
+    #                 access_control=model.get("info", {})
+    #                 .get("meta", {})
+    #                 .get("access_control", {}),
+    #             ):
+    #                 filtered_models.append(model)
+    #             continue
+
+    #         model_info = Models.get_model_by_id(model["id"])
+    #         if model_info:
+    #             if user.id == model_info.user_id or has_access(
+    #                 user.id, type="read", access_control=model_info.access_control
+    #             ):
+    #                 filtered_models.append(model)
+    #     models = filtered_models
+
+    log.debug(
+        f"/api/models returned filtered models accessible to the user: {json.dumps([model['id'] for model in models])}"
+    )
+
+    return {"data": models}
 
 @app.get("/api/models/base")
 async def get_base_models(user=Depends(get_admin_user)):
